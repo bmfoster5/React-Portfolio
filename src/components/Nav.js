89d30a3d0b9138e38bc1f8bca620ade34cf1e82f
bmfoster5 <foster.brandon96@gmail.com>
2023-03-16 14:19:45 -0400

    removed error

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,58 +1,40 @@
 import React from 'react';
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 
-const styles = {
-    navbarStyle: {
-      background: 'green',
-      justifyContent: 'flex-end',
-    },
-  };
 
-  function Navbar() {
-    return (
-      <nav style={styles.navbarStyle} className="navbar">
-        <a href="/">Welcome</a>
-      </nav>
-    );
-  }
-  
-  
 
 class Nav extends React.Component {
     render() {
-  return (
-   <div>
-<h2>This is the nav page</h2>
-<ul>
-<li className="listPadding">
-    <Link onClick={this.handleScroll}
-        to="home"
-        activeClass="active"
-        spy={true} 
-        smooth={true}
-    >
-        Home
-    </Link>
-  </li>
+        return (
+            <div>
+                <h2>This is the nav page</h2>
+                <ul>
+                    <li className="listPadding">
+                        <Link onClick={this.handleScroll}
+                            to="home"
+                            activeClass="active"
+                            spy={true}
+                            smooth={true}
+                        >
+                            Home
+                        </Link>
+                    </li>
 
-  <li className="listPadding">
-    <Link onClick={this.handleScroll}
-        to="resume"
-        activeClass="active"
-        spy={true} 
-        smooth={true}
-    >
-        resume
-    </Link>
-  </li>
-</ul>
+                    <li className="listPadding">
+                        <Link onClick={this.handleScroll}
+                            to="resume"
+                            activeClass="active"
+                            spy={true}
+                            smooth={true}
+                        >
+                            resume
+                        </Link>
+                    </li>
+                </ul>
+            </div>
 
-
-   </div>
-
-  );
-  }
+        );
+    }
 }
 
 export default Nav;
-export default Navbar;
